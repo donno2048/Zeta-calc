892122d652219d7a3c363c416742101496412b10
Elisha Hollander <just4now666666@gmail.com>
2021-02-06 20:30:36 +0200

    Faster method

--- __init__.py
+++ __init__.py
@@ -1,19 +1,17 @@
-def zeta(num,MAX): #MAX is accuracy and this calculates ζ(num)
-    factors=[0 for i in range(MAX)];P=[];C=[]
-    for i in range(2,MAX):
-        q=100*i/MAX
-        if int(q)==q:print(f'{int(q)}%')
-        if i==MAX-1:print('100%')
-        if i not in C:
-            P+=[i];factors[i]=1
-            for j in range(i,MAX,i):C+=[j]
-        else:
+from math import factorial
+def zeta(num, MAX): #MAX is accuracy and this calculates ζ(num)
+    factors, P = [0 for i in range(MAX)], []
+    for i in range(2, MAX):
+        q = 100 * i / MAX
+        if int(q) == q: print(f'{int(q)}%')
+        if i == MAX - 1: print('100%')
+        if (factorial(i - 1) + 1) % i: # If not prime
             for p in P:
-                k=i
-                for j in range(MAX):
-                    k/=p
-                    if int(k)!=k:break
-                factors[i]=max(j,factors[i])
-                if p>=MAX**2:break
-    return MAX/(sum([factors.count(i) for i in range(num)]))
-print(zeta(3,100))#to print out ζ(3) with low accuracy
+                i2, c = i, 0
+                while not i2 % p: c, i2 = c + 1, i2 // p
+                factors[i] = max(c, factors[i])
+        else:
+            P += [i]
+            factors[i] = 1
+    return MAX / (sum([factors.count(i) for i in range(num)]))
+print(zeta(3, 10000)) # To print out ζ(3) with low accuracy
